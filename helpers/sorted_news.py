82c843af93ad93d4559a9a6ff9d1d54309f738ba
Vinayak Mundakkal <playstorepointsusa@gmail.com>
2021-12-05 18:13:56 +0530

    finally

--- helpers/sorted_news.py
+++ helpers/sorted_news.py
@@ -29,7 +29,6 @@ english_list = [
     "lokmat-times-epaper-pdf",
     "kashmir-times-epaper-pdf"
 ]
-
 english_name = [
     "The Hindu Analysis",
     "Business Line",
@@ -62,3 +61,290 @@ english_name = [
     "Kashmir Times",
     None
 ]
+
+
+
+
+hindi_list = [
+    "amar-ujala-newspaper",
+    "dainik-jagran-newspaper-pdf",
+    "hindustan-dainik-newspaper",
+    "the-pioneer-newspaper",
+    "prabhat-khabar-newspaper",
+    "jansatta-newspaper",
+    "navbharat-times",
+    "rajasthan-patrika-newspaper",
+    "dainik-navajyoti-epaper-pdf",
+    "punjab-kesari-newspaper-pdf",
+    "dainik-bhaskar",
+    "business-standard-newspaper-download",
+    "hari-bhoomi-epaper-pdf",
+    "samachar-jagat-epaper",
+    "rashtriya-sahara-epaper",
+    "nai-dunia-epaper",
+    "lokmat-samachar-epaper",
+    "lok-tej-epaper",
+    "divya-himachal-epaper",
+    "deshbandhu-epaper",
+    "uttam-hindu-epaper"
+]
+hindi_name = [
+    "Amar Ujala",
+    "Dainik Jagran",
+    "Hindustan Dainik",
+    "The Pioneer",
+    "Prabhat Khabar",
+    "Jansatta",
+    "Navbharat Times",
+    "Rajasthan Patrika",
+    "Dainik Navajyoti",
+    "Punjab Kesari",
+    "Dainik Bhaskar",
+    "Business Standard",
+    "Hari Bhoomi",
+    "Samachar Jagat",
+    "Rashtriya Sahara",
+    "Nai Dunia",
+    "Lokmat Samachar",
+    "Lok Tej",
+    "Divya Himachal",
+    "Deshbandhu",
+    "Uttam Hindu",
+    None
+]
+
+
+
+telugu_list = [
+    "eenadu-newspaper",
+    "sakshi-newspaper-download",
+    "andhra-jyothi-newspaper-download",
+    "namasthe-telangana-epaper-pdf",
+    "nava-telangana-newspaper",
+    "mana-telangana-epaper-pdf",
+    "vaartha-newspaper-download",
+    "prabha-newspaper-download",
+    "manam-newspaper-download",
+    "surya-newspaper-download",
+    "andhra-bhoomi-newspaper",
+    "aadab-hyderabad-epaper",
+    "prajasakti-epaper",
+    "leader-epaper",
+    "visalaandhra-epaper",
+    "velugu-epaper"
+]
+telugu_name = [
+    "Eenadu",
+    "Sakshi",
+    "Andhra Jyothi",
+    "Namasthe Telangana",
+    "Nava Telangana",
+    "Mana Telangana",
+    "Vaartha",
+    "Prabha",
+    "Manam",
+    "Surya",
+    "Andhra Bhoomi",
+    "Aadab Hyderabad",
+    "Prajasakti",
+    "Leader",
+    "Visalaandhra",
+    "Velugu",
+    None
+]
+
+
+
+marathi_list = [
+    "loksatta-newspaper",
+    "saamana-epaper-pdf",
+    "lokmat-newspaper",
+    "sakal-newspaper",
+    "divya-marathi-epaper-pdf",
+    "maharashtra-times-epaper-pdf",
+    "tarun-bharat-epaper-pdf",
+    "pudhari-epaper-pdf"
+]
+marathi_name = [
+    "Loksatta",
+    "Saamana",
+    "Lokmat",
+    "Sakal",
+    "Divya Marathi",
+    "Maharashtra Times",
+    "Tarun Bharat",
+    "Pudhari",
+    None
+]
+
+
+
+bengali_list = [
+    "anandabazar-patrika-newspaper",
+    "uttarbanga-sambad-newspaper",
+    "bartaman-patrika-epaper-pdf",
+    "aajkaal-epaper-pdf",
+    "dainik-statesman-epaper-pdf",
+    "ekdin-epaper",
+    "ei-samay-epaper-pdf",
+    "ganashakti-epaper-pdf",
+    "sangbad-pratidin-epaper"
+]
+bengali_name = [
+    "Anandabazar Patrika",
+    "Uttarbanga Sambad",
+    "Bartaman Patrika",
+    "Aajkaal",
+    "Dainik Statesman",
+    "Ekdin",
+    "Ei Samay",
+    "Ganashakti",
+    "Sangbad Pratidin",
+    None
+]
+
+
+
+gujarati_list = [
+    "gujarat-samachar-epaper",
+    "sandesh-newspaper-download",
+    "divya-bhaskar-epaper-pdf",
+    "mid-day-epaper"
+]
+gujarati_name = [
+    "Gujarat Samachar",
+    "Sandesh",
+    "Divya Bhaskar",
+    "Mid Day",
+    None
+]
+
+
+
+punjabi_list = [
+    "punjabi-tribune-epaper",
+    "ajit-punjabi-epaper"
+]
+punjabi_name = [
+    "Punjabi Tribune",
+    "Ajit Punjabi",
+    None
+]
+
+
+
+tamil_list = [
+    "dinamalar-newspaper-pdf",
+    "dinakaran-epaper-pdf",
+    "dinamani-epaper-pdf",
+    "dina-thanthi",
+    "the-hindu-tamil-epaper"
+]
+tamil_name = [
+    "Dinamalar",
+    "Dinakaran",
+    "Dinamani",
+    "Dina Thanthi",
+    "The Hindu Tamil",
+    None
+]
+
+
+
+malayalam_list = [
+    "malayala-manorama-epaper",
+    "mathrubhumi-epaper",
+    "madhyamam-epaper-pdf",
+    "chandrika-epaper-pdf",
+    "deshabhimani-epaper-pdf",
+    "suprabhaatham-epaper-pdf",
+    "deepika-epaper-pdf"
+]
+malayalam_name = [
+    "Malayala Manorama",
+    "Mathrubhumi",
+    "Madhyamam",
+    "Chandrika",
+    "Deshabhimani",
+    "Suprabhaatham",
+    "Deepika",
+    None
+]
+
+
+
+kannada_list = [
+    "vijayavani-epaper-pdf",
+    "vijaya-karnataka-epaper-pdf",
+    "udayavani-epaper-pdf",
+    "prajavani-epaper-pdf"
+]
+kannada_name = [
+    "Vijayavani",
+    "Vijaya Karnataka",
+    "Udayavani",
+    "Prajavani",
+    None
+]
+
+
+
+urdu_list = [
+    "inquilab-epaper",
+    "siasat-daily-epaper"
+]
+urdu_name = [
+    "Inquilab",
+    "Siasat Daily",
+    None
+]
+
+
+
+odiya_list = [
+    "dharitri-epaper",
+    "sambad-epaper",
+    "samaja-epaper",
+    "orissapost-epaper-pdf",
+    "pragativadi-epaper-pdf",
+    "prameya-epaper-pdf"
+]
+odiya_name = [
+    "Dharitri",
+    "Sambad",
+    "Samaja",
+    "Orissapost",
+    "Pragativadi",
+    "Prameya",
+    None
+]
+
+
+
+assamese_list = [
+    "assam-tribune-epaper-pdf",
+    "asomiya-pratidin-epaper",
+    "dainik-assam-epaper",
+    "dainik-janambhumi-epaper",
+    "niyomiya-barta-epaper",
+    "amar-asom-epaper-pdf"
+]
+assamese_name = [
+    "Assam Tribune",
+    "Asomiya Pratidin",
+    "Dainik Assam",
+    "Dainik Janambhumi",
+    "Niyomiya Barta",
+    "Amar Asom",
+    None
+]
+
+
+
+
+
+
+
+
+
+    
